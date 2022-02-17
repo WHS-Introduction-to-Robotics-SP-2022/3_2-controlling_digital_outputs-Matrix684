1aae2f7316f3c2a7de2af560319378bfd8981c34
Matrix684 <99276073+Matrix684@users.noreply.github.com>
2022-02-17 09:38:28 -0700

    Create 3.2-controlling_digital_output.ino

---
+++ 3.2-controlling_digital_output.ino
@@ -0,0 +1,33 @@
+/*
+
+*/
+
+void setup() {
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+  delay(200);
+  digitalWrite(8, LOW);
+  delay(200);
+  digitalWrite(8, HIGH);
+  delay(200);
+  digitalWrite(8, LOW);
+  delay(1000);
+  digitalWrite(7, HIGH);
+  delay(200);
+  digitalWrite(7, LOW);
+  delay(200);
+  digitalWrite(7, HIGH);
+  delay(200);
+  digitalWrite(7, LOW);
+  delay(3000);
+}
+
+void loop() {
+  digitalWrite(8, HIGH);
+  delay(650);
+  digitalWrite(8, LOW);
+  digitalWrite(7, HIGH);
+  delay(650);
+  digitalWrite(7, LOW);
+}
